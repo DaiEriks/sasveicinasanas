606e08e8386efe6ff03a7abd6e0e32997211f7d7
Eriks Miska <116160809+DaiEriks@users.noreply.github.com>
2022-10-19 11:06:19 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+#Autors: Ēriks Miška
+#Datums: 19/10/22
+# Programma, kas sasveicinās ar lietotāju
+
+print ("HELLO WORLD")
+text="HAHAHAHHA"
+print ("Goooooooooooooood")
+print (76*76)
+vards=input("Lūdzu ievadi savu vārdu un nospied ENTER")
+print(vards[-1])
+if vards:
+  print("Got the name")
+if vards[0] == "S":
+  print("tu esi leja")
+  
+print("tu esi augsa")
